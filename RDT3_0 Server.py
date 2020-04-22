cab3365e01f4026b3fbef764e7b4da797c18953f
Chuggalump <62820049+Chuggalump@users.noreply.github.com>
2020-04-22 17:24:50 -0400

    SR Start
    Dictionary added, SNumber and SeqNum changed to be ints instead of bytes. Beginning work on different if statements for received packets

--- RDT3_0 Server.py
+++ RDT3_0 Server.py
@@ -45,13 +45,15 @@ start_time = time.time()
 x = False
 indexNumber = 0
 writeDataPacket = 0
-SNumber = b'\x00\x00'
+SNumber = 0
 oldSeqNum = SNumber
 file = open(FileName, 'wb')
 print("Ready to download file ...")
 ackPacket = bytearray()
 recvPacket = bytearray()
 final_handshake = False
+
+received_Queue = {}
 
 dat_error = 0
 ack_loss_rate = 0
@@ -74,6 +76,7 @@ def ack_loss(test_num):
     if rand_num < test_num:
         # Packet is "lost". Simulate by not sending the ACK back to the client
         print("ACK lost")
+        pass
     elif rand_num > test_num:
         # ACK packet isn't lost, send the ACK
         serverSocket.sendto(ackPacket, clientAddress)
@@ -96,21 +99,20 @@ def data_error(data):
             corrupted_data.append(i)
         for j in corrupt:
             corrupted_data.append(j)
-        print("corrupted_data length", len(corrupted_data))
         return corrupted_data
     else:
         return data
 
 
 while 1:
-
     if not final_handshake:
         # Wait for the packet to come from the client
         recvPacket, clientAddress = serverSocket.recvfrom(2048)
 
         # Parse data
         SeqNum = recvPacket[:2]
-        print('Packet received. Received SeqNum is: ', SeqNum)
+        SeqNum = int.from_bytes(SeqNum, byteorder='big')
+        print('Packet received. Received SeqNum is:', SeqNum)
         clientChecksum = recvPacket[2:4]
         dataPacket = recvPacket[4:]
 
@@ -121,26 +123,30 @@ while 1:
         # Convert Server-side checksum into bytes
         sbitsum = serverChecksum.to_bytes(2, byteorder='little')
 
-        # If checksums the same and SeqNum = 0, everything is right with the world
+        ackPacket = bytearray()
+        SNumber = SNumber.to_bytes(2, byteorder='big')
+        # Append the ACK to the packet
+        ackPacket.extend(SNumber)
+        SNumber = int.from_bytes(SNumber, byteorder='big')
+        print("Send packet after Seq Num =", ackPacket)
+        for i in sbitsum:
+            ackPacket.append(i)
+
+        # If checksums the same and SeqNum is what we expect, everything is right with the world
         if sbitsum == clientChecksum and SeqNum == SNumber:
-            ackPacket = bytearray()
-            # Append the ACK to the packet
-            ackPacket.extend(SNumber)
-            print("Send packet after Seq Num =", ackPacket)
 
-            for i in sbitsum:
-                ackPacket.append(i)
-
+            # Send the ACK packet back to sender, calculating if there's loss or not
             ack_loss(ack_loss_rate)
 
+            # SeqNum is correct, buffer data to the Queue
+            received_Queue[SeqNum] = dataPacket
+
             # Write the dataPacket received to the file
-            file.write(dataPacket)
+            file.write(received_Queue[SeqNum])
             print("Packet #", indexNumber, "Downloaded")
 
-            SNumber = int.from_bytes(SNumber, byteorder='big')
             print("Previous SeqNum was:", SNumber)
             SNumber += 1
-            SNumber = SNumber.to_bytes(2, byteorder='big')
             print('Updated SeqNum is: ', SNumber)
             indexNumber += 1
 
@@ -149,11 +155,14 @@ while 1:
                 final_handshake = True
 
         # If checksums different, there was data error, if SeqNum is 1 then there's ACK error
-        else:
+        elif SeqNum > SNumber:
+            # Packet is out of order, send ACK for this packet and buffer data
             ack_loss(ack_loss_rate)
+            received_Queue[SeqNum] = dataPacket
     else:
         recvPacket, clientAddress = serverSocket.recvfrom(2048)
         SeqNum = recvPacket[:2]
+        SeqNum = int.from_bytes(SeqNum, byteorder='big')
         if SeqNum == SNumber:
             file.close()
             break
